4818a6cc045e54c7d18a36a019e5d9beafd62a74
Pavel <63298371+Oskaorka@users.noreply.github.com>
2020-10-08 13:02:11 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,68 +1,4 @@
 $(document).ready(function () {
-    // $('.carousel__inner').slick({
-    //     speed: 1200,
-    //     /*adaptiveHeight: true,*/
-    //     prevArrow: "<button type='button' class='slick-prev'><img src='../img/slide/right.svg'></button>",
-    //     nextArrow: '<button type="button" class="slick-next"><img src="../img/slide/left.svg"></button>',
-    //     responsive: [{
-    //         breakpoint: 992,
-    //         settings: {
-    //             dots: true,
-    //             arrows: false
-    //         }
-    //     }]
-    // });
-
-    // $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
-    //     $(this)
-    //         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
-    //         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active')
-    //         .eq($(this).index()).addClass('catalog__content_active');
-    // });
-
-    // function toggleSlide(item) {
-    //     $(item).each(function (i) {
-    //         $(this).on('click', function (e) {
-    //             e.preventDefault();
-    //             $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-    //             $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-    //         });
-    //     });
-    // }
-
-    // toggleSlide('.catalog-item__link');
-    // toggleSlide('.catalog-item__back');
-
-    //modal window
-    // $('[data-modal=consultation]').on('click', function () {
-    //     $('.overlay, #consultation').fadeIn('slow');
-    // });
-    // $('.modal__close').on('click', function () {
-    //     $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
-    // });
-    // $('.button_mini').each(function (i) {
-    //     $(this).on('click', function () {
-    //         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
-    //         $('.overlay, #order').fadeIn('slow');
-    //     });
-    // });
-
-    // $('form').submit(function (e) {
-    //     e.preventDefault();
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "mailer/smart.php",
-    //         data: $(this).serialize()
-    //     }).done(function () {
-    //         $(this).find("input").val("");
-    //         $('#consultation, #order').fadeOut();
-    //         $('.overlay,#thanks').fadeIn('slow');
-
-    //         $('form').trigger('reset');
-    //     });
-    //     return false;
-    // });
-
 
     //Smooth scroll and pageup
 
@@ -75,7 +11,7 @@ $(document).ready(function () {
 
     });
 
-    $("a[href=#]").click(function () {
+    $("a[href=#up]").click(function () {
         const _href = $(this).attr("href");
         $("html, body").animate({
             scrollTop: $(_href).offset().top + "px"
@@ -83,8 +19,6 @@ $(document).ready(function () {
         return false;
     });
 
-
-    // });
     let emailBt = document.querySelector('.email-bt');
     let callBt = document.querySelector('.callback-bt');
 
@@ -226,4 +160,6 @@ $(document).ready(function () {
         });
 
     });
+
+    $('input[name=phone]').mask("+7(999) 999-999");
 });
